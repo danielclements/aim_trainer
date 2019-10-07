0c4f6b387ff210fe3a89530a6f0e2daed0a7d1d4
Daniel Clements <daniel.clements14@gmail.com>
2019-10-07 21:20:09 +0100

    added hit % as a stat and changed theme colors

--- assets/js/game-logic.js
+++ assets/js/game-logic.js
@@ -1,15 +1,26 @@
+// Variables 
+
+var hitCounter = "";
+var missCounter = "";
+var hitPercentage = "";
+var gameState = false;
+var gameOutCome = "";
+var playerName = "Player_1"
 
 
 function targetSpawner(){
     var numberGen = Math.floor((Math.random() * 12) + 1);
 
 
-    var spawnPoint=  '#spawn-point-'.concat(numberGen);
+    var spawnPoint=  "#spawn-point-".concat(numberGen,) ;
 
-
-    $(spawnPoint).append(`<div class="target" onclick="targetSpawner()"></div>`);
+  
+    $(spawnPoint).append('<div class="target" onclick="targetSpawner()"></div>');
 
 
     console.log(numberGen);
     console.log(spawnPoint)
 };
+
+
+targetSpawner();
