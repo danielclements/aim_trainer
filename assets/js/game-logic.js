f64c650a90f0b3e3167f88383dfa795dab7f8823
Daniel Clements <daniel.clements14@gmail.com>
2019-10-07 22:51:48 +0100

    Added spawner function that adds a target and removes it after 1500ms

--- assets/js/game-logic.js
+++ assets/js/game-logic.js
@@ -1,18 +1,21 @@
 // Variables 
 
-var hitCounter = "";
-var missCounter = "";
-var hitPercentage = "";
+var hitCounter ;
+var missCounter ;
+var hitPercentage ;
 var gameState = false;
-var gameOutCome = "";
+var gameOutCome ;
 var playerName = "Player_1"
 
 
+
+// Spawner 
 function targetSpawner(){
+    $(".target").remove();
     var numberGen = Math.floor((Math.random() * 12) + 1);
 
 
-    var spawnPoint=  "#spawn-point-".concat(numberGen,) ;
+    var spawnPoint=  "#spawn-point-".concat(numberGen) ;
 
   
     $(spawnPoint).append('<div class="target" onclick="targetSpawner()"></div>');
