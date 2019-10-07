9259ad2d1334e2ae649ae32538a5de3b1cf43bcb
Daniel Clements <daniel.clements14@gmail.com>
2019-10-07 22:58:28 +0100

    added a timeout function to remove the spanwed target after 1500ms and add a new one

--- assets/js/game-logic.js
+++ assets/js/game-logic.js
@@ -1,8 +1,12 @@
+$(document).ready(function() {
+    spawnLoop();
+    });
 // Variables 
 
 var hitCounter ;
 var missCounter ;
 var hitPercentage ;
+var spawnCount = 0;
 var gameState = false;
 var gameOutCome ;
 var playerName = "Player_1"
@@ -10,20 +14,41 @@ var playerName = "Player_1"
 
 
 // Spawner 
+
+
 function targetSpawner(){
     $(".target").remove();
+
+    
     var numberGen = Math.floor((Math.random() * 12) + 1);
 
 
     var spawnPoint=  "#spawn-point-".concat(numberGen) ;
 
   
-    $(spawnPoint).append('<div class="target" onclick="targetSpawner()"></div>');
+    $(spawnPoint).append('<div class="target" onclick="targetSpawner(); "></div>');
 
+    spawnCount++;
 
-    console.log(numberGen);
-    console.log(spawnPoint)
+    
+    
+    console.log(spawnCount);
 };
 
 
-targetSpawner();
+//calls the Target spawner function every 1.5 seconds if the taret is missed , resets on click
+function spawnLoop(){
+    setTimeout(function(){
+        targetSpawner();
+        spawnLoop();
+    },1500);
+ }
+
+
+
+
+
+
+
+
+
