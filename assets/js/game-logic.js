74a29083f5b52f9525eb3a7ebdd0d3abe865c9fe
Daniel Clements <daniel.clements14@gmail.com>
2019-10-08 11:33:20 +0100

    Added a function that records successful hits and updates in the stat section with every successful hit

--- assets/js/game-logic.js
+++ assets/js/game-logic.js
@@ -1,54 +1,59 @@
-$(document).ready(function() {
+$(document).ready(function () {
     spawnLoop();
-    });
+});
 // Variables 
 
-var hitCounter ;
-var missCounter ;
-var hitPercentage ;
+var hitCounter = 0;
+var missCounter;
+var hitPercentage;
 var spawnCount = 0;
 var gameState = false;
-var gameOutCome ;
+var gameOutCome;
 var playerName = "Player_1"
 
 
+const hitDom = $("#hit-counter")
 
 // Spawner 
 
 
-function targetSpawner(){
+function targetSpawner() {
+
+
     $(".target").remove();
 
-    
+
     var numberGen = Math.floor((Math.random() * 12) + 1);
 
 
-    var spawnPoint=  "#spawn-point-".concat(numberGen) ;
+    var spawnPoint = "#spawn-point-".concat(numberGen);
 
-  
-    $(spawnPoint).append('<div class="target" onclick="targetSpawner(); "></div>');
+
+    $(spawnPoint).append('<div class="target" onclick="targetSpawner(); hitCounter++ ; successfulHit() "></div>');
+
 
     spawnCount++;
 
-    
-    
+
+
+
     console.log(spawnCount);
+
 };
 
 
-//calls the Target spawner function every 1.5 seconds if the taret is missed , resets on click
-function spawnLoop(){
-    setTimeout(function(){
+//calls the Target spawner function every 1.5 seconds if the target is missed , resets on click
+function spawnLoop() {
+    setTimeout(function () {
         targetSpawner();
         spawnLoop();
-    },1500);
- }
+    }, 1500);
+}
 
+// hit Counter function to be called when a tarhet is clicked on 
 
+function successfulHit(){
 
+    hitDom.text(hitCounter) 
 
-
-
-
-
-
+}
