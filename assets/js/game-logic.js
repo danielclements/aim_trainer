84edca1c2ca663f213ce3a336507a4b11f2f1d00
Daniel Clements <daniel.clements14@gmail.com>
2019-10-08 20:53:01 +0100

    implemented a if statment to stop spawning targets after 30 spawns, slight bug in miss counter as a result of this code

--- assets/js/game-logic.js
+++ assets/js/game-logic.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
 var hitCounter = 0;
 
 
-var spawnCount = 0;
+var spawnCount = 22;
 var gameState = false;
 var gameOutCome;
 var playerName = "Player_1";
@@ -14,37 +14,70 @@ var playerName = "Player_1";
 const targetId = $("#targetIcon");
 const hitDom = $("#hit-counter");
 const missDom = $("#missed-counter");
-const accuracyDom= $("#accuracy-counter")
+const accuracyDom = $("#accuracy-counter")
 
 // Spawner 
 
 
 function targetSpawner() {
 
+    if (spawnCount < 29) {
 
-    $(".target").remove();
-
-
-    var numberGen = Math.floor((Math.random() * 12) + 1);
+        killSwitch();
 
 
-    var spawnPoint = "#spawn-point-".concat(numberGen);
+        var numberGen = Math.floor((Math.random() * 12) + 1);
 
 
-    $(spawnPoint).append('<div class="target" onclick= "targetSpawner(); hitCounter++ ;successfulHit();"   id="targetIcon"></div>');
+        var spawnPoint = "#spawn-point-".concat(numberGen);
 
 
-    spawnCount++;
+        $(spawnPoint).append('<div class="target" onclick= "targetSpawner(); hitCounter++ ;successfulHit(); "   id="targetIcon"></div>');
 
-    failedHit();
-    
-    accuracyCalc();
 
+        spawnCount++;
+
+        failedHit();
+
+        accuracyCalc();
+        console.log(spawnCount);
+
+
+
+    } else if (spawnCount <30) {
+
+
+        killSwitch();
+
+
+        var numberGen = Math.floor((Math.random() * 12) + 1);
+
+
+        var spawnPoint = "#spawn-point-".concat(numberGen);
+
+        $(spawnPoint).append(`<div class="target" onclick= "hitCounter++ ;successfulHit(); killSwitch();"   id="targetIcon"></div>`);
+
+
+        spawnCount++;
+
+        failedHit();
+
+        accuracyCalc();
+       
+        console.log(spawnCount);
+        
+
+    } else {
+        killSwitch();
+    }
 
 
 };
 
+function killSwitch() {
+    $(".target").remove();
 
+};
 
 
 //calls the Target spawner function every 1.5 seconds if the target is missed 
@@ -55,7 +88,9 @@ function spawnLoop() {
     }, 1500);
 };
 
-// functions to record successful hits / every time a target is missed 
+
+
+// functions to record successful hits / every time a target is missed / Accuracy calc 
 
 function successfulHit() {
 
@@ -71,7 +106,10 @@ function failedHit(num1, num2) {
 
     var missCounter = num1 - num2;
 
-    missDom.text(missCounter);
+    if (spawnCount <= 30) {
+        missDom.text(missCounter);
+    }
+
 
 };
 
@@ -82,6 +120,6 @@ function accuracyCalc(hits, spawned) {
 
     var accuracyTotal = hits * 100 / spawned;
     var accuracyRounded = Math.round(accuracyTotal * 100) / 100;
-    
+
     accuracyDom.text(accuracyRounded, '%')
 };
