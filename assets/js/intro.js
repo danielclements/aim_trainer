9351a480ce6b5b2d67781202a962a3865f552b1c
Daniel Clements <daniel.clements14@gmail.com>
2019-10-14 23:56:05 +0100

    added intro music intro page, stops after you press play

--- assets/js/intro.js
+++ assets/js/intro.js
@@ -1,5 +1,7 @@
 $( document ).ready(function() {
  
+    introMusic.play();
+
    setInterval(() => {
        targetSpawner();
        
--- assets/js/settings.js
+++ assets/js/settings.js
@@ -1,21 +1,21 @@
-$( document ).ready(function() {
+$(document).ready(function () {
+
+    
+
     if (localStorage.getItem("theme") == "light") {
 
         lightSwitch();
-    }
+        
+    } else if (localStorage.getItem("theme") == "dark") {
 
-    else if (localStorage.getItem("theme") == "dark") {
 
-    
         darkSwitch();
     };
 
     if (localStorage.getItem("audio") == "off") {
 
         audioOff();
-    }
-
-    else if (localStorage.getItem("audio") == "On") {
+    } else if (localStorage.getItem("audio") == "On") {
 
         audioOn();
 
@@ -28,6 +28,7 @@ $( document ).ready(function() {
 
 var introMusic = new Audio();
 introMusic.src = "assets/audio/273539__tristan-lohengrin__8bit-introduction.wav";
+introMusic.volume = 0.09;
 
 var gunSound = new Audio();
 gunSound.src = "assets/audio/404795__owlstorm__retro-video-game-sfx-laser-2.wav";
@@ -43,7 +44,7 @@ menuSound.src = "assets/audio/350863__cabled-mess__blip-c-03.wav";
 
 // Themes 
 
-function lightSwitch(){
+function lightSwitch() {
     localStorage.setItem("theme", "light");
 
     $("body").css("background-color", "#ffdede");
@@ -59,7 +60,7 @@ function lightSwitch(){
 }
 
 
-function darkSwitch(){
+function darkSwitch() {
     localStorage.setItem("theme", "dark");
 
     $("body").css("background-color", "");
@@ -72,26 +73,29 @@ function darkSwitch(){
     $(".index-header").removeClass("index-header-light");
     $(".about-para").addClass("white");
     $(".rules-header").addClass("white");
-    
+
 }
 
 
 // audio toggle 
 
-function audioOff(){
+function audioOff() {
     localStorage.setItem("audio", "off");
 
     $("#audioOff").removeClass("btn-secondary").addClass("btn-danger");
     $("#audioOn").removeClass("btn-success").addClass("btn-secondary");
     hitSound.volume = 0;
+    introMusic.volume = 0;
 }
 
 
-function audioOn(){
+function audioOn() {
     localStorage.setItem("audio", "on");
 
     $("#audioOff").addClass("btn-secondary").removeClass("btn-danger");
     $("#audioOn").addClass("btn-success").removeClass("btn-secondary");
 
+
+    introMusic.volume = 0.09;
     hitSound.volume = 0.09;
 }
