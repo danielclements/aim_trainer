9ccbb8190e2ba6a7d21735c1861d265920e20836
Daniel Clements <daniel.clements14@gmail.com>
2019-10-14 17:43:19 +0100

    improved the intro page and added functionality to the Play button git push

---
+++ assets/js/intro.js
@@ -0,0 +1,37 @@
+$( document ).ready(function() {
+ 
+   setInterval(() => {
+       targetSpawner();
+       
+   }, 1000);
+    
+
+    
+
+    console.log( "ready!" );
+});
+
+
+function targetSpawner() {
+
+
+    killSwitch();
+
+    var numberGen = Math.floor((Math.random() * 132) + 1);
+
+
+    var spawnPoint = "#spawn-point-".concat(numberGen);
+
+
+    $(spawnPoint).append('<div class="intro-target" id="targetIcon"> <i class="fas fa-bullseye target"></i></div>');
+
+
+    
+    console.log(numberGen);
+}
+
+
+function killSwitch() {
+    $("#targetIcon").remove();
+
+};
